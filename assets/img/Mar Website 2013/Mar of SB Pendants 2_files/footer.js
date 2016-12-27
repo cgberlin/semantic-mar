80f936450e8183790252bf9e3b011c6d5dca5eec
cgberlin <codyberlin@gmail.com>
2016-12-26 19:19:07 -0800

    almost finished landing page layout. Slider now working completely, changed size of main image. Need to fix some white space issues, navbar scrollto, animations, and contact at the bottom

---
+++ assets/img/Mar Website 2013/Mar of SB Pendants 2_files/footer.js
@@ -0,0 +1,6 @@
+// footer.js
+// stores the name to put into the footer copyright
+var addCopyright = false;
+var footerName = "Greg Geyer";
+if (addCopyright)
+document.write(" by " + footerName + " &#169; 2013 ");

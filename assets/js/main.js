9f343b81ca323f3659099693ba247d491c00492f
cgberlin <codyberlin@gmail.com>
2016-12-28 22:51:51 -0800

    fixed animations and added functionality to the home button

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,13 +21,6 @@ $(document).ready(function() {
         $('.ui.sidebar').sidebar('hide');
     });
 
-    $(window).scroll(function() {
-        var height = $(window).scrollTop();
-        if (height > 100) {
-            $('#section-two .grid').addClass('animated slideInUp');
-        }
-    });
-
     $('#toggle').click(function() {
         $('.ui.sidebar').sidebar('toggle');
     });
@@ -38,16 +31,31 @@ $(document).ready(function() {
         });
     });
 
-    $('#see-pendants').on('click', function(e){
+    $('#see-pendants, #phone-dropdown-pendant, #dropdown-pendant').on('click', function(e){
         e.preventDefault();
-        TweenMax.to($('.pusher'), .5, 
+        TweenMax.to($('.main-landing'), .5, 
             {x:+800, autoAlpha: 0, display:'none', onComplete:showCollection($('.ui.raised.card'))});
+        $('#pendants').show();
     });
 
     function showCollection(Name) {
-        $('#pendants').show();
+        $('.ui.rasied.card').css('visiblity', 'visible');
         TweenMax.staggerFromTo(Name, .5, 
             {x:-800, opacity:0},
-            {x:0, opacity:1}, .1);
+            {x:0, autoAlpha:1, visibility:'visible'}, .1);
+    }
+    $('#home-button, #home-logo').on('click', function(e){
+        e.preventDefault();
+        if ($('.main-landing').is(':visible') == false){
+            hideAndShow($('#pendants'), showHome());
+     }
+    });
+    function hideAndShow(HideThis, ShowThis){
+        HideThis.hide();
+        ShowThis();
+    }
+    function showHome(){
+        TweenMax.to($('.main-landing'), .5, 
+            {x:0, autoAlpha: 1, display:'block'});
     }
 });

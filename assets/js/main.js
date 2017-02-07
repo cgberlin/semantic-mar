dd316cc8acd96b4963c0970dd0f107c16abf445e
cgberlin <codyberlin@gmail.com>
2017-02-06 23:13:04 -0800

    fixed mobile issues with collections and featherlight

--- assets/js/main.js
+++ assets/js/main.js
@@ -444,12 +444,22 @@ $(document).ready(function() {
 
     function showTheSelectedCollection(CollectionArray, ModelArray, Name) {
             $('#collection-display').empty();
-            $('#collection-display').append('<h1 id = "collection-header">'+Name+'</h1><div id = "card-holder" class = "ui six stackable cards">');
-            for (var i =0, itsLength = CollectionArray.length; i < itsLength; i++){
-                $('#card-holder').append('<a href="'+CollectionArray[i]+'" data-featherlight="image"><div class = "ui raised stackable card">'+
-                        '<div class = "image" style = "background-image:url('+CollectionArray[i]+')"></div>'+
-                        '<div class="content"><p class = "center">'+ModelArray[i]+'</div></div></a>');
+            if(window.innerWidth <= 800) {                                                   //checks if mobile to display the appropriate card styles
+              $('#collection-display').append('<h1 id = "collection-header">'+Name+'</h1><div id = "card-holder" class = "ui two cards">');
+              for (var i =0, itsLength = CollectionArray.length; i < itsLength; i++){
+                  $('#card-holder').append('<a href="'+CollectionArray[i]+'" data-featherlight="image"><div class = "ui raised stackable card">'+
+                          '<div class = "image"><img src="'+CollectionArray[i]+'"/></div>'+
+                          '<div class="content"><p class = "center">'+ModelArray[i]+'</div></div></a>');
+              }
+            } else {
+              $('#collection-display').append('<h1 id = "collection-header">'+Name+'</h1><div id = "card-holder" class = "ui six stackable cards">');
+              for (var i =0, itsLength = CollectionArray.length; i < itsLength; i++){
+                  $('#card-holder').append('<a href="'+CollectionArray[i]+'" data-featherlight="image"><div class = "ui raised stackable card">'+
+                          '<div class = "image" style = "background-image:url('+CollectionArray[i]+')"></div>'+
+                          '<div class="content"><p class = "center">'+ModelArray[i]+'</div></div></a>');
+              }
             }
+
     }
 
     $('#home-button, #home-logo, #home-button-mobile').on('click', function(e){
